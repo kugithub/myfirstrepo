53957a5978dd5c95a4c03b038293d9f630b52ed0
venkatesh kunchenapalli <vkuchenapalli@apple.com>
2018-09-14 10:55:47 -0700

    changes

---
+++ client/src/main/java/com/apple/wwrc/service/customer/CustomerServiceInterface.java
@@ -0,0 +1,22 @@
+package com.apple.wwrc.service.customer;
+
+import com.apple.wwrc.foundation.framework.annotation.ApiParam;
+import com.apple.wwrc.foundation.framework.annotation.POSApi;
+import com.apple.wwrc.foundation.framework.annotation.POSMethod;
+import com.apple.wwrc.foundation.framework.annotation.POSService;
+import com.apple.wwrc.foundation.framework.exception.FrameworkException;
+import com.apple.wwrc.service.customer.model.response.EmployeeSearch;
+
+/**
+ *  This is your service common class. Make sure it's documented here.
+ */
+@POSService(name="CustomerService", version="v1.0")
+public interface CustomerServiceInterface {
+
+	// exposed public APIs (Will e availabe at gateway)
+	@POSApi(name="searchEmployee", desc="Employee lookup api.")
+	@POSMethod()
+	public EmployeeSearch employeeLookup(@ApiParam(value="query", desc="encrypted badgeId or email address")String query,
+								 @ApiParam(value="page", desc="page number (begin with 0)")int page,
+								 @ApiParam(value="limit", desc="page size") int limit) throws FrameworkException;
+}
---
+++ service/src/main/java/com/apple/wwrc/service/customer/resolver/AbstractUserLookup.java
@@ -0,0 +1,20 @@
+package com.apple.wwrc.service.customer.resolver;
+
+import com.apple.wwrc.service.customer.model.response.DSUser;
+import com.apple.wwrc.service.customer.model.response.EmployeeSearch;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public abstract class AbstractUserLookup {
+    public abstract DSUser lookupFromEmail(String query, int page, int limit) throws Exception;
+    public abstract DSUser lookupFromBadge(String query, int page, int limit) throws Exception;
+
+    public EmployeeSearch makeSearchResponse(int page, int limit, DSUser dsUser) {
+        if (dsUser == null) {
+            return new EmployeeSearch(page, limit, new ArrayList<DSUser>());
+        } else {
+            return new EmployeeSearch(page, limit, new ArrayList<DSUser>(Arrays.asList(dsUser)));
+        }
+    }
+    public void tearDown() {}
+}
